6e96a9555d5a92880467ffd0c8a2b67f726f1e72
Sanjay <sanj9069@gmail.com>
2019-03-17 15:50:59 +0530

    new-files

---
+++ precache-manifest.44ab199f8b33f6fbde1c2e40514acee7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "b2b72ae77718f92a18dd",
+    "url": "/static/js/main.b2b72ae7.chunk.js"
+  },
+  {
+    "revision": "eb95d78fd52838d93780",
+    "url": "/static/js/2.eb95d78f.chunk.js"
+  },
+  {
+    "revision": "b2b72ae77718f92a18dd",
+    "url": "/static/css/main.09fbcf56.chunk.css"
+  },
+  {
+    "revision": "eb95d78fd52838d93780",
+    "url": "/static/css/2.03449135.chunk.css"
+  },
+  {
+    "revision": "73446b10088d24eca4fb5d8cf567d3af",
+    "url": "/index.html"
+  }
+];
